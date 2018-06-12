c9fabf101b3aa42aa4fb003780c66b90f6d259d9
brendon-nguyen <btn28@cornell.edu>
2018-06-11 22:50:49 -0500

    designing state, adding constantsfiles

--- state.py
+++ state.py
@@ -1,2 +1,25 @@
 # Brendon Nguyen, Jack Sparkman
 # June 11, 2018
+
+"""
+state
+- character : Character
+    - current hp
+    - max hp
+    - mutable attributes
+- rooms : Room list
+    - rooms are revealed as player explores
+- current_room : Room
+- enemies_in_room : Enemy list
+- items_in_room : Item list
+- inventory : Item dictionary
+    - keys : Item
+    - bombs : Item
+    - coins : Item
+    - trinket : Item
+    - spacebar_item : Item
+    - consumable : Item
+- game_over : bool
+    - final boss hp = 0 or current hp = 0
+    - final boss hp = 0 and current hp != 0 
+"""
