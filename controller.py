b5bc3f12af5e21b8f1558b9ce50802823893c0a2
brendon-nguyen <btn28@cornell.edu>
2018-06-24 15:40:55 -0500

    commenting questionable code

--- controller.py
+++ controller.py
@@ -11,39 +11,39 @@ is_blue = True
 x = 30
 y = 30
 
-def update_face(x, y, done):
-    done = False
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            is_blue = not is_blue
-
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_w]: y -= 3
-    if pressed[pygame.K_s]: y += 3
-    if pressed[pygame.K_a]: x -= 3
-    if pressed[pygame.K_d]: x += 3
-    if pressed[pygame.K_UP]:
-        continue
-
-    screen.fill((0, 0, 0))
-    BLUE = (0, 128, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    color_circ = (255,220,210)
-    pygame.draw.circle(screen, color_circ, [x + 40, y + 40], 40)
-    pygame.draw.circle(screen, BLUE, [x + 60, y + 40], 10)
-    pygame.draw.circle(screen, BLUE, [x + 20, y + 40], 10)
-    pygame.draw.circle(screen, BLACK, [x + 60, y + 40], 4)
-    pygame.draw.circle(screen, BLACK, [x + 20, y + 40], 4)
-    pygame.draw.ellipse(screen, RED, [x + 28, y + 50, 25, 15], 4)
-    pygame.draw.ellipse(screen, color_circ, [x + 28, y + 54, 25, 15])
-    pygame.display.flip()
-    clock.tick(60)
+# def update_face(x, y, done):
+#     done = False
+#     while not done:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 done = True
+#
+# while not game_over:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             done = True
+#         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+#             is_blue = not is_blue
+#
+#     pressed = pygame.key.get_pressed()
+#     if pressed[pygame.K_w]: y -= 3
+#     if pressed[pygame.K_s]: y += 3
+#     if pressed[pygame.K_a]: x -= 3
+#     if pressed[pygame.K_d]: x += 3
+#     if pressed[pygame.K_UP]:
+#         continue
+#
+#     screen.fill((0, 0, 0))
+#     BLUE = (0, 128, 255)
+#     BLACK = (0, 0, 0)
+#     RED = (255, 0, 0)
+#     color_circ = (255,220,210)
+#     pygame.draw.circle(screen, color_circ, [x + 40, y + 40], 40)
+#     pygame.draw.circle(screen, BLUE, [x + 60, y + 40], 10)
+#     pygame.draw.circle(screen, BLUE, [x + 20, y + 40], 10)
+#     pygame.draw.circle(screen, BLACK, [x + 60, y + 40], 4)
+#     pygame.draw.circle(screen, BLACK, [x + 20, y + 40], 4)
+#     pygame.draw.ellipse(screen, RED, [x + 28, y + 50, 25, 15], 4)
+#     pygame.draw.ellipse(screen, color_circ, [x + 28, y + 54, 25, 15])
+#     pygame.display.flip()
+#     clock.tick(60)
--- view.py
+++ view.py
@@ -8,20 +8,19 @@ screen = settings.screen
 x = settings.FACE_X
 y = settings.FACE_Y
 
-
 screen.fill((0, 0, 0))
 BLUE = (0, 128, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 color_circ = (255, 220, 210)
 
-def draw_face():
-    pygame.draw.circle(screen, color_circ, [x + 40, y + 40], 40)
-    pygame.draw.circle(screen, BLUE, [x + 60, y + 40], 10)
-    pygame.draw.circle(screen, BLUE, [x + 20, y + 40], 10)
-    pygame.draw.circle(screen, BLACK, [x + 60, y + 40], 4)
-    pygame.draw.circle(screen, BLACK, [x + 20, y + 40], 4)
-    pygame.draw.ellipse(screen, RED, [x + 28, y + 50, 25, 15], 4)
-    pygame.draw.ellipse(screen, color_circ, [x + 28, y + 54, 25, 15])
-    pygame.display.flip()
-    clock.tick(60)
+# def draw_face():
+#     pygame.draw.circle(screen, color_circ, [x + 40, y + 40], 40)
+#     pygame.draw.circle(screen, BLUE, [x + 60, y + 40], 10)
+#     pygame.draw.circle(screen, BLUE, [x + 20, y + 40], 10)
+#     pygame.draw.circle(screen, BLACK, [x + 60, y + 40], 4)
+#     pygame.draw.circle(screen, BLACK, [x + 20, y + 40], 4)
+#     pygame.draw.ellipse(screen, RED, [x + 28, y + 50, 25, 15], 4)
+#     pygame.draw.ellipse(screen, color_circ, [x + 28, y + 54, 25, 15])
+#     pygame.display.flip()
+#     clock.tick(60)
